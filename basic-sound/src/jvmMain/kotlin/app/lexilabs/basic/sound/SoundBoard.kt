55274bfd9004c188be3ef494d0819b78f5ad73bb
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-05-27 21:47:37 -0400

    Updated powerUp in JVM to account for various extension types.  It's a bit much...

--- basic-sound/src/jvmMain/kotlin/app/lexilabs/basic/sound/SoundBoard.kt
+++ basic-sound/src/jvmMain/kotlin/app/lexilabs/basic/sound/SoundBoard.kt
@@ -5,9 +5,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
-import java.io.File
+import java.net.URI
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.UnsupportedAudioFileException
 
 public actual class SoundBoard actual constructor (context: Any?): SoundBoardBuilder {
 
@@ -20,9 +21,38 @@ public actual class SoundBoard actual constructor (context: Any?): SoundBoardBui
 
     public actual override fun powerUp() {
         Log.i(tag, "launch:starting to load sounds")
-        soundBytes.forEachIndexed { index, soundByte ->
-            audioInputStreams[soundByte.name] =
-                AudioSystem.getAudioInputStream(File(soundByte.localPath).absoluteFile)
+        soundBytes.forEach { soundByte ->
+            Log.d(tag, "${soundByte.name} -> PATH=${soundByte.localPath}")
+            // Check for generated.resources
+            if (soundByte.localPath.contains(".generated.resources")) {
+                val path = soundByte.localPath.substringAfter(".generated.resources/")
+                val stream = object {}.javaClass.getResourceAsStream(path)
+                audioInputStreams[soundByte.name] = AudioSystem.getAudioInputStream(stream)
+            } else if (
+                // Check for Local or absolute path resources
+                soundByte.localPath.startsWith("file:", true) ||
+                soundByte.localPath.startsWith("/", true)) {
+                val stream = object {}.javaClass.getResourceAsStream(soundByte.localPath)
+                    ?: throw Exception("Resource not found: ${soundByte.localPath}")
+                audioInputStreams[soundByte.name] = AudioSystem.getAudioInputStream(stream)
+            // Check for website
+            } else if (
+                    soundByte.localPath.startsWith("https:", true) ||
+                    soundByte.localPath.startsWith("http:", true)
+                ) {
+                audioInputStreams[soundByte.name] = AudioSystem.getAudioInputStream(
+                    URI(soundByte.localPath).toURL()
+                )
+            // Check if links to file inside the JAR
+            } else if (soundByte.localPath.startsWith("jar:", true)) {
+                val path = soundByte.localPath.substringAfter("!/")
+                val stream = object {}.javaClass.getResourceAsStream("/$path")
+                        ?: throw Exception("Resource not found: $path")
+                audioInputStreams[soundByte.name] = AudioSystem.getAudioInputStream(stream)
+            }
+            else {
+                throw UnsupportedAudioFileException("Unclear if this is a file or link")
+            }
         }
         startMixer()
         Log.i(tag, "launch:complete")
