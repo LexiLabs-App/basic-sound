462f2d613862907510fc312f8433e54975b2d756
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2026-08-19 23:25:34 -0400

    Removed Compose Multiplatform plugin and dependencies

--- basic-sound/build.gradle.kts
+++ basic-sound/build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.multiplatform.library)
     alias(libs.plugins.kotlinx.binary.compatibility.validator)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
     alias(libs.plugins.dokka)
     alias(libs.plugins.kover)
 }
@@ -70,10 +68,6 @@ kotlin {
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.coroutines.test)
             implementation(libs.lexilabs.basic.logging)
-            compileOnly(libs.compose.foundation)
-            compileOnly(libs.compose.runtime)
-            api(libs.compose.foundation)
-            api(libs.compose.runtime)
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
